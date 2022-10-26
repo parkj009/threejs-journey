5dde6361bcdaaebe30090a62ac14a0e4ce61d3ef
James Park <parkj009@gmail.com>
2022-10-25 14:12:11 -1000

    06 completed

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,32 +1,68 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Time
+// let time = Date.now();
+
+// Clock
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+
+// Animations
+const loop = () => {
+  // Time
+  // Time comparison is used to have the same speed / rotation regardless of FPS
+  //   const currentTime = Date.now();
+  //   const deltaTime = currentTime - time;
+  //   time = currentTime;
+
+  // Clock
+  // Same as time, but using threejs builtin clock method
+  //   const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  //   mesh.rotation.y += 0.0002 * deltaTime;
+  //   camera.position.x = Math.cos(elapsedTime);
+  //   camera.position.y = Math.sin(elapsedTime);
+  //   camera.lookAt(mesh.position);
+  //   mesh.rotation.y = elapsedTime * Math.PI * 2;
+
+  // Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(loop);
+};
+
+loop();
